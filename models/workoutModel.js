e3cf863ac5eb5e0c9e0388b300eb402f89fa0d21
Umut Alptekin <amacalptekin@gmail.com>
2020-11-24 01:06:06 +0300

    created model files, server file

---
+++ models/workoutModel.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    // look at seeder for format
+    // two items: day & excersize
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [{
+        type: {
+            type: String,
+            require: true,
+        },
+        name: {
+            type: String,
+            require: true,
+        },
+        duration: {
+            type: Number,
+            require: true,
+        },
+        weight: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        distance: {
+            type: Number
+        }
+    }]
+})
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
